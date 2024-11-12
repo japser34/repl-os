28a1e84b0db2af78714d83166a9de3a25a40c059
japser34 <100087296+japser34@users.noreply.github.com>
2024-11-12 15:19:48 +0100

    Added error handeling for tools/print.go

--- tools/print.go
+++ tools/print.go
@@ -5,8 +5,13 @@ import(
 	"time"
 )
 func TypePrint(speed int, text string) {
-	for _, char := range text {
-		fmt.Print(string(char))
-		time.Sleep(time.Duration(speed) * time.Millisecond)
+	if speed<0 {
+		speed = 0
+	}
+	if if len(s) > 0 {
+		for _, char := range text {
+			fmt.Print(string(char))
+			time.Sleep(time.Duration(speed) * time.Millisecond)
+		}
 	}
 }
