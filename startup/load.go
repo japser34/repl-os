97940e1b95191dd4283e3c4ae8f296026c2d382d
japser34 <100087296+japser34@users.noreply.github.com>
2024-11-12 14:56:35 +0100

    Simplified starup/startup

--- startup/load.go
+++ startup/load.go
@@ -10,18 +10,16 @@ func Start() {
 	asciiArt := `
   _____  ______ _____  _
  |  __ \|  ____|  __ \| |
- | |__) | |__  | |__) | |  ______ ___  ___
- |  _  /|  __| |  ___/| | |______/ _ \/ __|
- | | \ \| |____| |    | |____   | (_) \__ \
- |_|  \_\______|_|    |______|   \___/|___/ `
-	goText := `Go-version`
+ | |__) | |__  | |__) | |  _____   ___  ___
+ |  _  /|  __| |  ___/| | |_____| / _ \/ __|
+ | | \ \| |____| |    | |____    | (_) \__ \
+ |_|  \_\______|_|    |______|    \___/|___/ `
+	goText := "Go-version \n \n"
 	
 	fmt.Print("Loading")
-	tools.TypePrint(500, "...")
-	fmt.Println()
+	tools.TypePrint(500, "...\n")
 
 	tools.TypePrint(15, asciiArt)
 	time.Sleep(150 * time.Millisecond)
 	tools.TypePrint(130, goText)
-	fmt.Println()
 }
