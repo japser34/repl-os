826edf117adcb49a9cbb6339dc1320da9536a480
japser34 <100087296+japser34@users.noreply.github.com>
2024-11-12 22:12:23 +0100

    Fixed bug in tools/print

--- tools/print.go
+++ tools/print.go
@@ -8,7 +8,7 @@ func TypePrint(speed int, text string) {
 	if speed<0 {
 		speed = 0
 	}
-	if if len(s) > 0 {
+	if len(s) > 0 {
 		for _, char := range text {
 			fmt.Print(string(char))
 			time.Sleep(time.Duration(speed) * time.Millisecond)
